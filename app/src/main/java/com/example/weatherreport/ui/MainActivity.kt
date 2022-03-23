c36461aa87698e4952e21a9b039c65c8669bf1dd
nOmeep <37767676+nOmeep@users.noreply.github.com>
2022-03-24 00:09:42 +0300

    add-yandex-api-interface

--- app/src/main/java/com/example/weatherreport/ui/MainActivity.kt
+++ app/src/main/java/com/example/weatherreport/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.weatherreport
+package com.example.weatherreport.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weatherreport.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/weatherreport/ui/MainFragment.kt
+++ app/src/main/java/com/example/weatherreport/ui/MainFragment.kt
@@ -1,5 +1,6 @@
-package com.example.weatherreport
+package com.example.weatherreport.ui
 
 import androidx.fragment.app.Fragment
+import com.example.weatherreport.R
 
 class MainFragment : Fragment(R.layout.fragment_main)
