8ee7467e249911c10669512b8b41103b9963d7dc
nOmeep <37767676+nOmeep@users.noreply.github.com>
2022-03-26 21:46:06 +0300

    add-api-key-storage

---
+++ app/src/main/java/com/example/weatherreport/data/api/WeatherAPI.kt
@@ -0,0 +1,14 @@
+package com.example.weatherreport.data.api
+
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface YandexWeatherAPI {
+    companion object {
+        const val BASE_URL = ""
+    }
+
+
+}
